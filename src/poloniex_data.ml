b1b479043ddfc8221d82b9a49256d778eb81ceed
Vincent Bernardoff <vb@luminar.eu.org>
2019-05-19 13:02:43 +0200

    move code elsewhere

--- src/poloniex_data.ml
+++ src/poloniex_data.ml
@@ -287,18 +287,6 @@ let reject_historical_price_data_request ?reason_code w (req : DTC.Historical_pr
     Logs.debug ~src (fun m -> m "-> HistoricalPriceData reject %s" reject_text)
   end k
 
-let span_of_interval = function
-  | `interval_10_seconds -> Time_ns.Span.of_int_sec 10
-  | `interval_1_day -> Time_ns.Span.of_day 1.
-  | `interval_1_minute -> Time_ns.Span.of_int_sec 60
-  | `interval_1_second -> Time_ns.Span.of_int_sec 1
-  | `interval_1_week -> Time_ns.Span.of_day 7.
-  | `interval_2_seconds -> Time_ns.Span.of_int_sec 2
-  | `interval_30_seconds -> Time_ns.Span.of_int_sec 30
-  | `interval_4_seconds -> Time_ns.Span.of_int_sec 4
-  | `interval_5_seconds -> Time_ns.Span.of_int_sec 5
-  | `interval_tick -> Time_ns.Span.zero
-
 let max_int_value = Int64.of_int_exn Int.max_value
 let start_key = Bytes.create 8
 
